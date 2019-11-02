e0a28f24534a6c341aecc6eeb8dd8da7ebc9b24e
khaancc <khaancc@gmail.com>
2019-11-02 23:14:28 +0100

    removed unused index files

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
+
 var tasks = require('./routes/tasks');
 
 var port = process.env.PORT || 3000;
@@ -10,7 +10,7 @@ var port = process.env.PORT || 3000;
 var app = express();
 
 //View Engine
-app.set('views', path.join(__dirname, 'views'));
+
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
